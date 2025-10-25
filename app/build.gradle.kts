548e5667093645cd86c6f5e9a8652ecee280431d
RamaSandhy <rshandy07@gmail.com>
2025-10-25 19:12:07 +0700

    perbaikan

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,7 +11,7 @@ android {
     defaultConfig {
         applicationId = "com.example.praktikum2"
         minSdk = 30
-        targetSdk = 34
+        targetSdk = 36
         versionCode = 1
         versionName = "1.0"
 
