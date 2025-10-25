75d4602472137e89e57d116ae2dce5e4387a5645
RamaSandhy <rshandy07@gmail.com>
2025-10-25 19:11:26 +0700

    Menambahkan teks copyright di bagian bawah tampilan dengan posisi BottomCenter

--- app/src/main/java/com/example/praktikum2/UIPertama.kt
+++ app/src/main/java/com/example/praktikum2/UIPertama.kt
@@ -1,70 +1,80 @@
+package com.example.composablelayout2
+
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.*
-import androidx.compose.material3.*
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.praktikum2.R
 
 @Composable
-fun AktivitasPertama(modifier: Modifier = Modifier) {
-    Column(
-        modifier = modifier
-            .fillMaxSize()
-            .padding(top = 100.dp),
+fun ActivitasPertama(modifier: Modifier){
+    Column (
+        modifier = Modifier
+            .padding(top = 100.dp)
+            .fillMaxSize(),
         horizontalAlignment = Alignment.CenterHorizontally
-    ) {
+    ){
         Text(
-            text = stringResource(id = R.string.prodi),
+            text = stringResource( id = R.string.prodi),
             fontSize = 35.sp,
             fontWeight = FontWeight.Bold
         )
-
         Text(
-            text = stringResource(id = R.string.univ),
+            text = stringResource( id = R.string.prodi),
             fontSize = 22.sp
         )
-
-        Spacer(modifier = Modifier.height(25.dp))
-        Card(
+        Spacer(modifier = Modifier.height(height = 25.dp))
+        Card (
             modifier = Modifier
-                .fillMaxWidth()
-                .padding(12.dp),
+                .fillMaxWidth(fraction = 1f)
+                .padding(all = 12.dp),
             colors = CardDefaults.cardColors(
                 containerColor = Color.DarkGray
             )
-        ) {
-            Row(
-                modifier = Modifier.padding(16.dp),
-                verticalAlignment = Alignment.CenterVertically
-            ) {
-                val gambar = painterResource(id = R.drawable.logoumy)
+        ){
+            Row(){
+                val gambar = painterResource( id = R.drawable.logoumy)
                 Image(
                     painter = gambar,
                     contentDescription = null,
                     modifier = Modifier
-                        .size(120.dp)
-                        .padding(5.dp)
+                        .size(size = 100.dp)
+                        .padding(all = 5.dp)
                 )
-                Spacer(modifier = Modifier.width(30.dp))
-
-                Column {
+                Spacer(modifier = Modifier.width(width = 30.dp))
+                Column() {
                     Text(
                         text = stringResource(id = R.string.nama),
-                        fontSize = 24.sp,
-                        fontWeight = FontWeight.Bold,
-                        color = Color.White
+                        fontSize = 30.sp,
+                        fontFamily = FontFamily.Cursive,
+                        color = Color.White,
+                        modifier = Modifier.padding(top = 15.dp)
                     )
                     Text(
                         text = stringResource(id = R.string.alamat),
-                        fontSize = 18.sp,
-                        color = Color.LightGray
+                        fontSize = 20.sp,
+                        color = Color.Yellow,
+                        modifier = Modifier.padding(top = 10.dp)
                     )
                 }
             }
@@ -72,7 +82,7 @@ fun AktivitasPertama(modifier: Modifier = Modifier) {
         Box(
             modifier = Modifier
                 .fillMaxSize()
-        ) {
+        ){
             Text(
                 text = stringResource(id = R.string.copy),
                 modifier = Modifier
@@ -82,5 +92,3 @@ fun AktivitasPertama(modifier: Modifier = Modifier) {
         }
     }
 }
-
-
